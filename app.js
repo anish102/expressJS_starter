b6d91d454ec50e22dd30e4cdec6d95cd823442ca
anish102 <anishkarki695@gmail.com>
2024-02-15 16:07:55 +0545

    Middlewares

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 var express = require("express");
+var http = require("http");
 
 var app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello world!");
+app.use((req, res, next) => {
+  console.log("In comes a request to:" + req.method + " to " + req.url);
+  next();
 });
 
-app.listen(3000, () => {
-  console.log("Express app started on port 3000.");
+app.use((req, res, next) => {
+  var minute = new Date().getMinutes();
+  if (minute % 2 === 0) {
+    next();
+  } else {
+    res.statusCode = 403;
+    res.end("Not authorized.");
+  }
 });
+
+app.use((req, res) => {
+  res.end("Secret info: the password is 'nothing'!");
+});
+
+http.createServer(app).listen(3000);
